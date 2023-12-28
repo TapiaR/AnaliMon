645d710e61fa0df26203930f7121e6d9280f0884
AlTa <atapiarubio@alumni.unav.es>
2023-12-28 19:44:19 +0100

    cambio del codigo del test unit

--- tests/test_unitario_analizador.py
+++ tests/test_unitario_analizador.py
@@ -1,65 +1,45 @@
-# Actualización del código de prueba unitaria con la nueva estructura de directorios
-# y la explicación de las librerías y funciones utilizadas.
-
-import os
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+from src.analizador_monedas import AnalizadorMonedas
 import pandas as pd
-from biblio_krk.analizador_monedas import AnalizadorMonedas, obtener_pares_disponibles
+import os
 
-# os: Librería estándar para interactuar con el sistema operativo, como leer rutas de archivo.
-# unittest: Framework de pruebas unitarias que proporciona una forma de organizar y ejecutar pruebas.
-# unittest.mock: Proporciona una forma de simular partes de tu sistema durante las pruebas.
-# pandas: Librería para manipulación y análisis de datos, especialmente útil para trabajar con tablas.
-
-# Aquí se carga el archivo CSV de prueba que se encuentra en el directorio de tests/data
-current_dir = os.path.dirname(__file__)
-csv_path = os.path.join(current_dir, 'data', 'XZECZEUR.csv')
-test_data = pd.read_csv(csv_path)
-
-# Clase de prueba para obtener_pares_disponibles
-class TestObtenerParesDisponibles(unittest.TestCase):
-
-    def test_retorno_lista(self):
-        # Prueba que la función devuelve una lista.
-        pares = obtener_pares_disponibles()
-        self.assertIsInstance(pares, list)
-
-    def test_lista_contiene_strings(self):
-        # Prueba que cada elemento de la lista es una cadena de caracteres.
-        pares = obtener_pares_disponibles()
-        for par in pares:
-            self.assertIsInstance(par, str)
-
-# Clase de prueba para AnalizadorMonedas
 class TestAnalizadorMonedas(unittest.TestCase):
 
     def setUp(self):
-        # Método que se ejecuta antes de cada prueba, estableciendo condiciones iniciales.
-        self.analizador = AnalizadorMonedas("XZECZEUR")
+        # Ruta al archivo CSV en la carpeta data
+        self.datos_prueba_csv = os.path.join(os.path.dirname(__file__), 'data', 'XZCEUR.csv')
+        self.analizador = AnalizadorMonedas("XZCEUR")
 
-    @patch('krk_biblio.analizador_monedas.AnalizadorMonedas.descargar_datos')
-    def test_descargar_datos(self, mock_descargar):
-        # Simulación de la descarga exitosa de datos con mocking.
-        mock_descargar.return_value = test_data
+    def cargar_datos_prueba(self):
+        # Cargar los datos de prueba desde el archivo CSV
+        return pd.read_csv(self.datos_prueba_csv)
+
+    @patch('src.analizador_monedas.krakenex.API')
+    def test_descargar_datos(self, mock_krakenex):
+        # Cargar datos de prueba desde el CSV
+        datos_prueba = self.cargar_datos_prueba()
+
+        # Crear un objeto MagicMock para simular la respuesta de la API
+        mock_response = MagicMock()
+        mock_response.return_value = {
+            'error': [],
+            'result': {
+                'XZCEUR': datos_prueba.values.tolist()  # Convertir los datos del DataFrame a una lista de listas
+            }
+        }
+        mock_krakenex.return_value.query_public = mock_response
+
+        # Ejecutar el método descargar_datos que debería utilizar los datos simulados
         self.analizador.descargar_datos()
+
+        # Verificaciones para asegurar que los datos se han cargado correctamente
         self.assertIsNotNone(self.analizador.datos)
+        self.assertIn('time', self.analizador.datos.columns)
+        self.assertEqual(len(self.analizador.datos), len(datos_prueba))
 
-    def test_calcular_estocastico_sin_datos(self):
-        # Prueba que calcular_estocastico lanza una excepción si no hay datos.
-        with self.assertRaises(ValueError):
-            self.analizador.calcular_estocastico()
 
-    def test_graficar_cotizaciones_sin_datos(self):
-        # Prueba que graficar_cotizaciones lanza una excepción si no hay datos.
-        with self.assertRaises(ValueError):
-            self.analizador.graficar_cotizaciones()
-
-    def test_graficar_estocastico_sin_datos(self):
-        # Prueba que graficar_estocastico lanza una excepción si no hay datos.
-        with self.assertRaises(ValueError):
-            self.analizador.graficar_estocastico()
-
-# Ejecución de las pruebas
 if __name__ == '__main__':
-    unittest.main(argv=[''], exit=False)
+    unittest.main()
+
+
